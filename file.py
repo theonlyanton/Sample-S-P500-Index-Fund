5ad189d647ff48d5bcc02939f67769d0c0f153c7
Anton Krotenok <80707699+theonlyanton@users.noreply.github.com>
2021-08-17 16:27:47 -0400

    Update file.py

--- file.py
+++ file.py
@@ -1,8 +1,6 @@
 import requests 
 import pandas
 
-
-
 page = pandas.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
 stocks_table = page[0]
 info = stocks_table.info()
@@ -12,7 +10,7 @@ print()
 stocks_columns = ["Ticker", "Price", "Market Capitalization", "Shares to Buy"]
 stocks_dataframe = pandas.DataFrame(columns = stocks_columns)
 
-#for 500 API calls
+#generating 500 individual API calls
 for symbol in stocks_table["Symbol"]:
   new_symbol = symbol
 
@@ -23,7 +21,7 @@ for symbol in stocks_table["Symbol"]:
   data = requests.get(api_url).json()
   stocks_dataframe = stocks_dataframe.append(pandas.Series([new_symbol, data["latestPrice"], data["marketCap"], "N/A"], index = stocks_columns), ignore_index = True)
   
-# for 5 API calls
+# generating 5 batch API calls
 def split_list(list, number_per_group):
   for index in range(0, len(list), number_per_group):
     yield list[index : index + number_per_group]
@@ -34,7 +32,6 @@ stock_symbols_strings = []
 for index in range(0, len(stock_symbols_groups)):
   stock_symbols_strings.append(",".join(stock_symbols_groups[index]))
 
-#batch API calls
 batch_stocks_dataframe = pandas.DataFrame(columns = stocks_columns)
 
 for symbol_string in stock_symbols_strings:
