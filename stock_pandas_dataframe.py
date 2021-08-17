26673430668d75096a9c2818b77632065a7f5e1b
Anton Krotenok <80707699+theonlyanton@users.noreply.github.com>
2021-08-17 16:46:47 -0400

    Update stock_pandas_dataframe.py

--- stock_pandas_dataframe.py
+++ stock_pandas_dataframe.py
@@ -1,6 +1,8 @@
 import requests 
 import pandas
 
+IEX_CLOUD_API_TOKEN = #enter your IEX Cloud API Sandbox token here
+
 page = pandas.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
 stocks_table = page[0]
 info = stocks_table.info()
